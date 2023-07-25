87edcc1fe25caf297f48b556794a1fb9d58c1343
crazyads69 <leminhtri11a1@gmail.com>
2023-07-25 10:20:15 +0700

    Fix script

--- start_sqlserver.sh
+++ start_sqlserver.sh
@@ -8,9 +8,9 @@ read -p "Input docker name: " NAME;
 read -p "Input port: " PORT;
 read -p "Input password: " PASSWORD;
 
-echo $NAME;
-echo $PORT;
-echo $PASSWORD;
+echo "Name docker: ${NAME}";
+echo "Port: ${PORT}";
+echo "Password: ${PASSWORD}";
 
 echo "Start docker: ";
 docker run -e ACCEPT_EULA=Y -e MSSQL_PID='Developer' -e MSSQL_SA_PASSWORD=${PASSWORD} -e MSSQL_TCP_PORT=${PORT} -p ${PORT}:${PORT} --name ${NAME} --hostname ${NAME} --platform linux/amd64  -d mcr.microsoft.com/mssql/server:2019-latest;
